18826f5ec894f7cbb61f44e657f7483e87d80fd3
Markus Mulkahainen <markus.mulkahainen@hotmail.com>
2018-04-13 16:42:43 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,106 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+const players = new Map();
+
+// SERVER MESSAGES TO CLIENT --------
+
+const START_GAME = { type: "SERVER_START" };
+const END_GAME = { type: "SERVER_END" };
+const JOIN = { type: "SERVER_JOIN" };
+const UPDATE = { type: "SERVER_UPDATE" };
+const FINISH = { type: "SERVER_FINISH" };
+const DIE = { type: "SERVER_DIE" };
+
+// ----------------------------------
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
+const newPlayer = ({ name }) => {
+  players.set(name, { map: 'Level1', location: [0, 0], lives: 5, score: 0 });
+  wss.broadcast(JSON.stringify({ ...JOIN, name }));
+};
+
+const getPlayers = () => {
+  let playerList = [];
+  players.forEach((player, name) => {
+    playerList.push({ name, ...player });
+  });
+
+  return playerList;
+};
+
+const startGame = () => {
+  wss.broadcast(JSON.stringify(START_GAME));
+};
+
+const endGame = () => {
+  wss.broadcast(JSON.stringify(END_GAME));
+};
+
+const updatePlayer = ({ name, map, location, lives, score }) => {
+  players.set(name, { map, location, lives, score });
+  wss.broadcast(JSON.stringify({ ...UPDATE, name, map, location, lives, score }));
+};
+
+const killPlayer = ({ name }) => {
+  wss.broadcast(JSON.stringify({ ...DIE, name }));
+};
+
+const finishPlayer = ({ name }) => {
+  wss.broadcast(JSON.stringify({ ...FINISH, name }));
+}
+
+wss.on('connection', function connection(ws) {
+  console.log("[INFO] New connection");
+  let name = "";
+
+  ws.on('message', function incoming(message) {
+    const msgParsed = JSON.parse(message);
+    const { type } = msgParsed;
+
+    if (!type) {
+      console.log(`[ERROR] Unknown message: ${message}`);
+    }
+
+    switch (type) {
+      case "END":
+        endGame();
+        break;
+      case "START":
+        startGame();
+        break;
+      case "JOIN":
+        name = msgParsed.name;
+        newPlayer(msgParsed);
+        ws.send(JSON.stringify(getPlayers()));
+        break;
+      case "UPDATE":
+        updatePlayer(msgParsed);
+        break;
+      case "DIE":
+        killPlayer(msgParsed);
+        break;
+      case "FINISH":
+        finishPlayer(msgParsed);
+        break;
+        
+      default:
+        console.log(`[ERROR] Unknown type: ${type}`);
+    }
+  });
+
+  ws.on('close', () => {
+    console.log(`[INFO] Player ${name} quit`);
+    players.delete(name);
+  });
+});
+
+console.log("[INFO] Server started");
